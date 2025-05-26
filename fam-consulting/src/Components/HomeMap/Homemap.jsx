1097fa05be230a5414c1af9fca7806565e96fe5a
zaurkhalilli <ggencetel274@gmail.com>
2025-05-26 15:17:00 +0400

    Map elave olundu

---
+++ fam-consulting/src/Components/HomeMap/Homemap.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../HomeMap/Homemap.scss'
+const Homemap = () => {
+  return (
+    <div id='map'>
+     <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6075.817609929147!2d49.861095!3d40.410871!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40307d55ed328cc9%3A0x4ac39f78263b079d!2zMjI0IMaPaG3JmWQgUsmZY8mZYmxpLCBCYWvEsSAxMDc1LCDQkNC30LXRgNCx0LDQudC00LbQsNC9!5e0!3m2!1sru!2sus!4v1744886793110!5m2!1sru!2sus" width="100%" height="500"  loading="lazy" referrerpolicy="no-referrer-when-downgrade"  title="FAM Group Construction office on Google Maps" frameborder="0">
+</iframe>
+    </div>
+  )
+}
+
+export default Homemap
